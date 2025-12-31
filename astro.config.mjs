bb294f09e164219791d0af7544a820dfd91112bc
LegalFingers <nyakoobian@gmail.com>
2025-12-31 14:01:47 -0800

    Troubleshoot site deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://legalfingers.github.io/flowers-site/',
+	base: '/flowers-site/'
 	integrations: [mdx(), sitemap()],
 });
